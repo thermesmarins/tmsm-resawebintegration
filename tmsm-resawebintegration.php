7d3a3adc7b20703200d0dee63db9caa7d733cbae
Nico Mollet <nico.mollet@gmail.com>
2017-08-31 10:11:16 +0200

    Require PHP version

--- tmsm-resawebintegration.php
+++ tmsm-resawebintegration.php
@@ -18,6 +18,7 @@
  * Version:           1.0.0
  * Author:            Nicolas Mollet
  * Author URI:        https://github.com/nicomollet
+ * Requires PHP:      5.6
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       tmsm-woocommerce-customadmin
